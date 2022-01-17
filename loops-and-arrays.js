fe15f0e8a9bea8732b9ad86cc4e73d2fa147c5e1
haris72 <haris.mujovic@gmail.com>
2022-01-17 10:31:21 +0100

    ready for pull request

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -5,6 +5,11 @@
 // ==========================================
 
 const names = ["Henk", "Piet", "Fred", "Joop"];
+console.log(names);
+for (let i = 0; i < names.length; i++) {
+    names[i] = names[i] + "je";
+}
+console.log(names);
 
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
@@ -22,6 +27,16 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 // ==========================================
 
 const numbers = [2, 4, 5, 29, 38];
+console.log(numbers);
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+        numbers[i] = numbers[i] * 2;
+    }
+    if (numbers[i] % 2 === 1) {
+        numbers[i] = numbers[i] * 3;
+        }
+}
+    console.log(numbers);
 
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
@@ -41,6 +56,14 @@ const numbers = [2, 4, 5, 29, 38];
 // ==========================================
 
 const squares = [30, 2, 8, 24, 11];
+console.log(squares);
+
+for (let i = 0; i < squares.length; i++) {
+    let x = squares[i];
+    squares[i] = Math.pow(x,3);
+    squares[i] = "Het volume van " + x + " is " + squares[i];
+}
+console.log(squares);
 
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
